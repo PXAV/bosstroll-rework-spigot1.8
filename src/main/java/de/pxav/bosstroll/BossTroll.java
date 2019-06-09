f885fb4e33ac29a53bffb26f3efcdc15bee32d7f
pxav <pxav.studios@gmail.com>
2019-06-09 15:07:15 +0200

    Implemented basic configuration file management

--- src/main/java/de/pxav/bosstroll/BossTroll.java
+++ src/main/java/de/pxav/bosstroll/BossTroll.java
@@ -1,13 +1,34 @@
 package de.pxav.bosstroll;
 
+import de.pxav.bosstroll.configuration.ConfigurationFile;
+import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
- * A class description goes here.
+ * This is the main class of the plugin.
  *
  * @author pxav
  */
+
+@Getter
 public class BossTroll extends JavaPlugin {
 
+    private String prefix = "§8[§cTroll§8] §r";
+    private String defaultFilePath = "plugins//BossTrollRework";
 
+    private ConfigurationFile configurationFile;
+
+    @Override
+    public void onEnable() {
+
+        // creates and loads the plugin configuration file
+        this.saveDefaultConfig();
+        this.configurationFile = new ConfigurationFile(this);
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
 }
---
+++ src/main/java/de/pxav/bosstroll/configuration/ConfigurationFile.java
@@ -0,0 +1,52 @@
+package de.pxav.bosstroll.configuration;
+
+import de.pxav.bosstroll.BossTroll;
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+
+/**
+ * This class handles the configuration file of the plugin.
+ *
+ * @author pxav
+ */
+public class ConfigurationFile {
+
+    // instane of the main class
+    private BossTroll main;
+
+    private File file;
+    private YamlConfiguration configuration;
+
+    /**
+     * Default constructor.
+     * Executes #loadFile() to create or load the configuration file.
+     * Executed #loadSettings() to load all settings to the cache.
+     *
+     * @param main The instance of your main class.
+     */
+    public ConfigurationFile(final BossTroll main) {
+        this.main = main;
+        this.loadFile();
+        this.loadSettings();
+    }
+
+    /**
+     * Initializes the file.
+     */
+    private void loadFile() {
+        this.file = new File(this.main.getDefaultFilePath(), "config.yml");
+        this.configuration = YamlConfiguration.loadConfiguration(file);
+        Bukkit.getConsoleSender().sendMessage(this.main.getPrefix() + "§aConfiguration file successfully loaded!");
+    }
+
+    /**
+     * Load all settings from the file.
+     */
+    private void loadSettings() {
+
+        Bukkit.getConsoleSender().sendMessage(this.main.getPrefix() + "§aSuccessfully copied configuration data to cache!");
+    }
+
+}
