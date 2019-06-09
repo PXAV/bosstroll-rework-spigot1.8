056bcf6875575d3949f42d5cfca58a27c8a1abce
pxav <pxav.studios@gmail.com>
2019-06-09 14:49:20 +0200

    Initial commit

---
+++ src/main/java/de/pxav/bosstroll/BossTroll.java
@@ -0,0 +1,13 @@
+package de.pxav.bosstroll;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * A class description goes here.
+ *
+ * @author pxav
+ */
+public class BossTroll extends JavaPlugin {
+
+
+}
